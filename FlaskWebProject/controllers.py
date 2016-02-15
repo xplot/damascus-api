3d0b0973842b82d90d9b041dd3850b31e520bc11
Ajadex Lopez Rodriguez <ajadex_lopez@ultimatesoftware.com>
2016-02-14 20:24:44 -0500

    Testing logging

--- FlaskWebProject/controllers.py
+++ FlaskWebProject/controllers.py
@@ -4,7 +4,10 @@ Routes for the damascus-api.
 
 from flask import render_template, jsonify
 from FlaskWebProject import app
+import logging
 
 @app.route('/')
 def home():
+    print "this is a test log"
+    logging.warn("Warning, log working!!!")
     return jsonify({"test": "hello"})
