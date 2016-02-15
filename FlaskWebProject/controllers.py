d725daf41ce32bebc6ca682f9362919fc2ba1e0f
Ajadex Lopez Rodriguez <ajadex_lopez@ultimatesoftware.com>
2016-02-14 19:12:05 -0500

    Cleaning up and support for running Flash locally

--- FlaskWebProject/controllers.py
+++ FlaskWebProject/controllers.py
@@ -2,12 +2,9 @@
 Routes for the damascus-api.
 """
 
-from datetime import datetime
 from flask import render_template, jsonify
 from FlaskWebProject import app
 
 @app.route('/')
-@app.route('/home')
 def home():
-    """Renders the home page."""
     return jsonify({"test": "hello"})
--- app.py
+++ app.py
@@ -1,10 +1,4 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-    return "Hello, World!"
+from FlaskWebProject import app
 
 if __name__ == '__main__':
     app.run(debug=True)
