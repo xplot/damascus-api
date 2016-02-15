e10f9caf985966858f07c932645cfaaf2eb70ddf
Ajadex Lopez Rodriguez <ajadex_lopez@ultimatesoftware.com>
2016-02-14 21:03:24 -0500

    Testing logging

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -5,6 +5,6 @@ The flask application package.
 from flask import Flask
 app = Flask(__name__)
 import logging
-logging.basicConfig(filename='example.log',level=logging.DEBUG)
+logging.basicConfig(filename='logs.txt',level=logging.DEBUG)
 
 import FlaskWebProject.controllers
